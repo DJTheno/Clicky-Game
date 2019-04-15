12557e5ed1f010f178c2f41b1d576cc6d96d7af5
Dylan Theno <djtheno@gmail.com>
2019-04-15 10:51:14 -0500

    Updates

---
+++ precache-manifest.9a512fd85ac957066c8c0c4f81d0e049.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "4952fcffb1dc75bf2d0ad29e9982fda2",
+    "url": "/Clicky-Game/static/media/lantern.4952fcff.jpg"
+  },
+  {
+    "revision": "f7d7e1264c22681cf16b",
+    "url": "/Clicky-Game/static/css/main.1965147a.chunk.css"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Clicky-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "ceb4965094418cf78caa496af46dc276",
+    "url": "/Clicky-Game/static/media/artemis.ceb49650.jpg"
+  },
+  {
+    "revision": "65210f56bc3a334268e12fac429c8126",
+    "url": "/Clicky-Game/static/media/flash2.65210f56.PNG"
+  },
+  {
+    "revision": "73691ca09c1dbd4a1d3297711bf7c26e",
+    "url": "/Clicky-Game/static/media/harley2.73691ca0.PNG"
+  },
+  {
+    "revision": "8cf492833e39b9c2a5e8c83b699f1680",
+    "url": "/Clicky-Game/static/media/joker.8cf49283.jpg"
+  },
+  {
+    "revision": "f7d7e1264c22681cf16b",
+    "url": "/Clicky-Game/static/js/main.97b35c95.chunk.js"
+  },
+  {
+    "revision": "975873126e90b6efa00bb79e056fb69c",
+    "url": "/Clicky-Game/static/media/nw2.97587312.jpg"
+  },
+  {
+    "revision": "ea855ad8014adf37032e6ff7f8529936",
+    "url": "/Clicky-Game/static/media/redarrow.ea855ad8.jpg"
+  },
+  {
+    "revision": "f9326ed145adc4ec61cf6525dfdbfc44",
+    "url": "/Clicky-Game/static/media/robin.f9326ed1.jpg"
+  },
+  {
+    "revision": "113cf65ed1f75f424886bf0297f7ae38",
+    "url": "/Clicky-Game/static/media/shazam.113cf65e.jpg"
+  },
+  {
+    "revision": "4f042018831d8d9059f81bfcadcec584",
+    "url": "/Clicky-Game/static/media/supes.4f042018.jpg"
+  },
+  {
+    "revision": "1e7ab3d47de6cb8eba381b34b1d14234",
+    "url": "/Clicky-Game/static/media/theflash.1e7ab3d4.jpg"
+  },
+  {
+    "revision": "c272a99095014bb5a687299e88f8834a",
+    "url": "/Clicky-Game/static/media/wonder.c272a990.jpg"
+  },
+  {
+    "revision": "c92c3bf5a7b1d5fefd01ce79aef41ce0",
+    "url": "/Clicky-Game/index.html"
+  }
+];
